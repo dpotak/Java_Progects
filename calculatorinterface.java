ff73a221478a134de46b51d355f03a84e023be23
Darja Potak <darjapotak248@gmail.com>
2025-02-23 23:59:22 +0200

    сщььше7

--- calculatorinterface.java
+++ calculatorinterface.java
@@ -14,12 +14,37 @@ public class calculatorinterface {
         frame.add(button);
 
         JButton button1 = new JButton("2");
-        button1.setBounds(100, 50, 55, 35);
+        button1.setBounds(90, 50, 55, 35);
         frame.add(button1);
 
         JButton button2 = new JButton("3");
-        button2.setBounds(170, 50, 55, 35);
+        button2.setBounds(150, 50, 55, 35);
         frame.add(button2);
+
+        JButton button3 = new JButton("4");
+        button3.setBounds(30, 90, 55, 35);
+        frame.add(button3);
+
+        JButton button4 = new JButton("5");
+        button4.setBounds(90, 90, 55, 35);
+        frame.add(button4);
+
+        JButton button5 = new JButton("6");
+        button5.setBounds(150, 90, 55, 35);
+        frame.add(button5);
+
+        JButton button6 = new JButton("7");
+        button6.setBounds(30, 130, 55, 35);
+        frame.add(button6);
+
+        JButton button7 = new JButton("8");
+        button7.setBounds(90, 130, 55, 35);
+        frame.add(button7);
+
+        JButton button8 = new JButton("9");
+        button8.setBounds(150, 130, 55, 35);
+        frame.add(button8);
+
 
         frame.setVisible(true);
     }
