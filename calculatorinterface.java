d401bb3e80c3ebb0a8cc1fd34295c3fb5e418972
Darja Potak <darjapotak248@gmail.com>
2025-02-23 23:05:47 +0200

    commit5

--- calculatorinterface.java
+++ calculatorinterface.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class calculatorinterface {
     public static void main(String[] args) {
         JFrame frame = new JFrame("My one window");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
+        frame.setSize(400, 300);  
+        frame.setLayout(null); 
+
+        JButton button = new JButton("Click me");
+        button.setBounds(100 , 10 , 100 , 100);
+        frame.add(button);
+
         frame.setVisible(true);
     }
 }
