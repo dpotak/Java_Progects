b2871009c3a552b648ecfcf65096ca1931873104
Darja Potak <darjapotak248@gmail.com>
2025-02-23 23:16:34 +0200

    commit6

--- calculatorinterface.java
+++ calculatorinterface.java
@@ -6,12 +6,20 @@ public class calculatorinterface {
     public static void main(String[] args) {
         JFrame frame = new JFrame("My one window");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);  
+        frame.setSize(400, 400);  
         frame.setLayout(null); 
 
-        JButton button = new JButton("Click me");
-        button.setBounds(100 , 10 , 100 , 100);
+        JButton button = new JButton("1");
+        button.setBounds(30 , 50 , 55 , 35);
         frame.add(button);
+
+        JButton button1 = new JButton("2");
+        button1.setBounds(100, 50, 55, 35);
+        frame.add(button1);
+
+        JButton button2 = new JButton("3");
+        button2.setBounds(170, 50, 55, 35);
+        frame.add(button2);
 
         frame.setVisible(true);
     }
