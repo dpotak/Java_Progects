759c16786537960822df6ba63f7d8804ba9f650b
Darja Potak <darjapotak248@gmail.com>
2025-02-23 22:15:57 +0200

    commit4

---
+++ calculatorinterface.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class calculatorinterface {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My one window");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+}
+
