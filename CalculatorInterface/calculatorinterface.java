3204de73f1bcb6874affb6610060cc88781bab60
Darja Potak <darjapotak248@gmail.com>
2025-02-24 15:55:28 +0200

    commit15

--- CalculatorInterface/calculatorinterface.java
+++ CalculatorInterface/calculatorinterface.java
@@ -11,59 +11,59 @@ public class calculatorinterface {
         frame.setLayout(null); 
 
         JButton button = new JButton("1");
-        button.setBounds(30 , 50 , 55 , 35);
+        button.setBounds(30 , 65 , 55 , 35);
         frame.add(button);
 
         JButton button1 = new JButton("2");
-        button1.setBounds(90, 50, 55, 35);
+        button1.setBounds(90, 65, 55, 35);
         frame.add(button1);
 
         JButton button2 = new JButton("3");
-        button2.setBounds(150, 50, 55, 35);
+        button2.setBounds(150, 65, 55, 35);
         frame.add(button2);
 
         JButton button3 = new JButton("4");
-        button3.setBounds(30, 90, 55, 35);
+        button3.setBounds(30, 105, 55, 35);
         frame.add(button3);
 
         JButton button4 = new JButton("5");
-        button4.setBounds(90, 90, 55, 35);
+        button4.setBounds(90, 105, 55, 35);
         frame.add(button4);
 
         JButton button5 = new JButton("6");
-        button5.setBounds(150, 90, 55, 35);
+        button5.setBounds(150, 105, 55, 35);
         frame.add(button5);
 
         JButton button6 = new JButton("7");
-        button6.setBounds(30, 130, 55, 35);
+        button6.setBounds(30, 145, 55, 35);
         frame.add(button6);
 
         JButton button7 = new JButton("8");
-        button7.setBounds(90, 130, 55, 35);
+        button7.setBounds(90, 145, 55, 35);
         frame.add(button7);
 
         JButton button8 = new JButton("9");
-        button8.setBounds(150, 130, 55, 35);
+        button8.setBounds(150, 145, 55, 35);
         frame.add(button8);
 
         JButton button9 = new JButton("+");
-        button9.setBounds(30, 170, 55, 35);
+        button9.setBounds(30, 185, 55, 35);
         frame.add(button9);
 
         JButton button10 = new JButton("-");
-        button10.setBounds(90, 170, 55, 35);
+        button10.setBounds(90, 185, 55, 35);
         frame.add(button10);
 
         JButton button11 = new JButton("*");
-        button11.setBounds(150, 170, 55, 35);
+        button11.setBounds(150, 185, 55, 35);
         frame.add(button11);
 
         JButton button12 = new JButton("/");
-        button12.setBounds(210 , 50 , 55 , 35);
+        button12.setBounds(210 , 65 , 55 , 35);
         frame.add(button12);
 
         JButton button13 = new JButton("=");
-        button13.setBounds(210 , 90 , 55 , 35);
+        button13.setBounds(210 , 105 , 55 , 35);
         frame.add(button13);
 
         frame.setVisible(true);
