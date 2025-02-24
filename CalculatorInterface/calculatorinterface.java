ff27a4bbf053e1b0325e6b1c55d5bd3adc51c34b
Darja Potak <darjapotak248@gmail.com>
2025-02-24 23:05:50 +0200

    commit23

--- CalculatorInterface/calculatorinterface.java
+++ CalculatorInterface/calculatorinterface.java
@@ -15,7 +15,7 @@ class Text extends JFrame {
 
 public class calculatorinterface {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("My one window");
+        JFrame frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);  
         frame.setLayout(null); 
