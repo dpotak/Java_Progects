d9e573fbf2d22400b0e3d44ad5eb22028e79708b
Darja Potak <darjapotak248@gmail.com>
2025-02-25 12:52:16 +0200

    commit234

--- CalculatorInterface/calculatorinterface.java
+++ CalculatorInterface/calculatorinterface.java
@@ -21,7 +21,7 @@ public class calculatorinterface {
         frame.setLayout(null); 
 
         JTextField display = new JTextField();
-        display.setBounds(30, 20, 320, 40); 
+        display.setBounds(30, 37, 320, 40); 
         display.setEditable(false);  
         frame.add(display);
 
@@ -29,63 +29,63 @@ public class calculatorinterface {
         frame.add(t.lbl1);
 
         JButton button = new JButton("1");
-        button.setBounds(30 , 65 , 55 , 35);
+        button.setBounds(30 , 85 , 55 , 35);
         frame.add(button);
 
         JButton button1 = new JButton("2");
-        button1.setBounds(90, 65, 55, 35);
+        button1.setBounds(90, 85, 55, 35);
         frame.add(button1);
 
         JButton button2 = new JButton("3");
-        button2.setBounds(150, 65, 55, 35);
+        button2.setBounds(150, 85, 55, 35);
         frame.add(button2);
 
         JButton button3 = new JButton("4");
-        button3.setBounds(30, 105, 55, 35);
+        button3.setBounds(30, 125, 55, 35);
         frame.add(button3);
 
         JButton button4 = new JButton("5");
-        button4.setBounds(90, 105, 55, 35);
+        button4.setBounds(90, 125, 55, 35);
         frame.add(button4);
 
         JButton button5 = new JButton("6");
-        button5.setBounds(150, 105, 55, 35);
+        button5.setBounds(150, 125, 55, 35);
         frame.add(button5);
 
         JButton button6 = new JButton("7");
-        button6.setBounds(30, 145, 55, 35);
+        button6.setBounds(30, 165, 55, 35);
         frame.add(button6);
 
         JButton button7 = new JButton("8");
-        button7.setBounds(90, 145, 55, 35);
+        button7.setBounds(90, 165, 55, 35);
         frame.add(button7);
 
         JButton button8 = new JButton("9");
-        button8.setBounds(150, 145, 55, 35);
+        button8.setBounds(150, 165, 55, 35);
         frame.add(button8);
 
         JButton button9 = new JButton("+");
-        button9.setBounds(30, 185, 55, 35);
+        button9.setBounds(30, 205, 55, 35);
         frame.add(button9);
 
         JButton button10 = new JButton("-");
-        button10.setBounds(90, 185, 55, 35);
+        button10.setBounds(90, 205, 55, 35);
         frame.add(button10);
 
         JButton button11 = new JButton("*");
-        button11.setBounds(150, 185, 55, 35);
+        button11.setBounds(150, 205, 55, 35);
         frame.add(button11);
 
         JButton button12 = new JButton("/");
-        button12.setBounds(210 , 65 , 55 , 35);
+        button12.setBounds(210 , 85 , 55 , 35);
         frame.add(button12);
 
         JButton button13 = new JButton("=");
-        button13.setBounds(210 , 105 , 55 , 35);
+        button13.setBounds(210 , 125 , 55 , 35);
         frame.add(button13);
 
         JButton button_delete = new JButton("Del");
-        button_delete.setBounds(210, 145, 55, 35);
+        button_delete.setBounds(210, 165, 55, 35);
         frame.add(button_delete);
 
 
